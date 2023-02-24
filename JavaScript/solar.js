5b0a1944ce8ab2f53eb3e5ef7ee9df0d27f5661e
SamCcode <115998847+SamCcode@users.noreply.github.com>
2023-02-24 09:05:36 +0100

    almost

--- JavaScript/solar.js
+++ JavaScript/solar.js
@@ -1,33 +1,34 @@
 let planetsWrapper = document.querySelector(".planets-wrapper");
-let planetList =[];
+let listOfAllPlanets =[];
 let searchWrapper = document.querySelector(".search-wrapper")
 let content = searchWrapper.innerHTML;
 
 // hämta data
-async function getPlanetsData (){
+async function fetchPlanetsData (){
     let data = await fetch('https://majazocom.github.io/Data/solaris.json')
     data = await data.json();
     return data;
 }
 
-async function generateClickableUI() {
-    let data = await getPlanetsData()
+// gör klickbara "planeter"
+async function renderPlanetsToUI() {
+    let data = await fetchPlanetsData()
     for (let i = 0; i < data.length; i++) {
-        planetList.push(data[i])
+        listOfAllPlanets.push(data[i]);
         let planetUi = document.createElement("aside");
         planetUi.classList.add("planets");
-        planetUi.setAttribute("id", data[i].name)
+        planetUi.setAttribute("id", data[i].name);
         planetUi.innerHTML = `<p class="hover-text">${data[i].name}</p>`
-        planetsWrapper.appendChild(planetUi)
+        planetsWrapper.appendChild(planetUi);
         planetUi.addEventListener("click", () =>{
-            localStorage.setItem("clickedPlanet", JSON.stringify(data[i]))
-            generateInfo(data[i]);
-            
+            localStorage.setItem("clickedPlanet", JSON.stringify(data[i]));
+            renderOnclickUi(data[i]);
         });
     }
 }
 
-function generateInfo(data) {
+// rendera info ifall planeterna klcikas på
+function renderOnclickUi(data) {
     searchWrapper.innerHTML = `
     <h1 class="planet__name">${data.name}</h1>
     <button class="back-x">&#10005</button>
@@ -38,7 +39,7 @@ function generateInfo(data) {
     <h3>Max temp:<br><span>${data.temp.day}</span></h3>
     <h3>Min temp:<br><span>${data.temp.night}</span></h3>
     </div>
-    <div class="mons">
+    <div class="moons">
     <h3>Månar: ${data.moons}</h3>
     </div>
     <a href="../html/info.html">mer info!</a>
@@ -49,95 +50,86 @@ function generateInfo(data) {
         addEventListenerToPlanetSearch()
         })
 }
-
+// gör texten "vill du söka på en planet" klickbar
 function addEventListenerToPlanetSearch(){
     let searchText = document.querySelector(".planet-search");
     searchText.addEventListener("click", () =>{
-        searchPlanet()
+        inputSearchPlanet()
     })
 }
-
-function searchPlanet() {
+// renderar UI till sökrutan samt hanterar sökningar
+function inputSearchPlanet() {
     searchWrapper.innerHTML = `
     <input type="text" id="search" style="text-transform: uppercase" placeholder="skriv och tryck enter">
     <span class="placeholder"></span> 
     `
     let input = document.querySelector("#search");
     input.addEventListener("keyup", (keyPress) => {
-    let inputValue = input.value;
-    console.log(inputValue.toUpperCase());
-    let inputSearch = document.querySelector(".placeholder");
-    for (i = 0; i < planetList.length; i++) {
-        if (inputValue.length === 0) {
-            inputSearch.innerHTML = ""
-        }
-        else if (inputValue.length <= 1 && inputValue.toUpperCase().charAt(0) === planetList[i].name.toUpperCase().charAt(0)){
-            inputSearch.innerHTML = planetList[i].name.toUpperCase();
+    let writtenInputChars = input.value;
+    let rightWordUi = document.querySelector(".placeholder");
+    for (i = 0; i < listOfAllPlanets.length; i++) {
+        if (writtenInputChars.length === 0) {
+            rightWordUi.innerHTML = ""
+        } 
+        else if (writtenInputChars.length <= 1 && writtenInputChars.toUpperCase().charAt(0) === listOfAllPlanets[i].name.toUpperCase().charAt(0)){
+        rightWordUi.innerHTML = listOfAllPlanets[i].name.toUpperCase();
             if (keyPress.keyCode === 13){
-                // console.log(inputSearch.innerHTML);
-                renderInfoToUi(planetList[i])
-                console.log("hej");
-            }
-            break
+                    localStorage.setItem("clickedPlanet", JSON.stringify(listOfAllPlanets[i]))
+                    renderSearchedInfoToUi(listOfAllPlanets[i])
+                }
+                break
         }
-        else if (inputValue.length > 1 && planetList[i].name.toUpperCase().includes(inputValue.toUpperCase())) {
-            inputSearch.innerHTML = planetList[i].name.toUpperCase();
+        else if (writtenInputChars.length > 1 && listOfAllPlanets[i].name.toUpperCase().includes(writtenInputChars.toUpperCase())) {
+            rightWordUi.innerHTML = listOfAllPlanets[i].name.toUpperCase();
             if (keyPress.keyCode === 13){
-                // console.log(planetList[i]);
-                renderInfoToUi(planetList[i])
-                console.log("jaaaaa");
+            localStorage.setItem("clickedPlanet", JSON.stringify(listOfAllPlanets[i]))
+            renderSearchedInfoToUi(listOfAllPlanets[i])
             }
-            
-            
         }
     }
-
-    if (inputSearch.innerHTML.toUpperCase().includes(inputValue.toUpperCase()) && keyPress.keyCode === 13){
-    } else if (keyPress.keyCode === 13){
-        wrongChars()
+    if (rightWordUi.innerHTML.toUpperCase().includes(writtenInputChars.toUpperCase()) && keyPress.keyCode === 13){
+    } 
+    else if (keyPress.keyCode === 13){
+        wrongCharsSearched()
     }
-    if (inputSearch.innerHTML.toUpperCase().includes(inputValue.toUpperCase())) {}
+    if (rightWordUi.innerHTML.toUpperCase().includes(writtenInputChars.toUpperCase())) {
+    }
     else {
-        inputSearch.innerHTML = "";
+        rightWordUi.innerHTML = "";
     }
         
-        })
-    }
+    })
+}
 
-
-
-
-function renderInfoToUi(searchPlanet) {
-    console.log(searchPlanet);
-    localStorage.setItem("searchPlanet", JSON.stringify(searchPlanet))
+// renderar information om den sökta planeten till UI 
+function renderSearchedInfoToUi(inputSearchPlanet) {
+    localStorage.setItem("inputSearchPlanet", JSON.stringify(inputSearchPlanet))
     searchWrapper.innerHTML = `
-    <h2>${searchPlanet.name}</h2>
+    <h2>${inputSearchPlanet.name}</h2>
     <button class="back-x">&#10005</button>
-    <p">${searchPlanet.desc}</p> 
+    <p">${inputSearchPlanet.desc}</p> 
     <a href="../html/info.html">Tryck här för mer info!</a>
         `
     let backX = document.querySelector(".back-x");
     backX.addEventListener("click", () => {
-        console.log("hej");
         searchWrapper.innerHTML = `
         <h1 class="planet-search">Vill du söka på en planet?</h1>
         `
         addEventListenerToPlanetSearch()
     })
 }
-
-function wrongChars() {
+// vad som händer om man söker fel
+function wrongCharsSearched() {
     searchWrapper.innerHTML = `
     <h2>väldigt ledsen men det du sökte var ingen planet...</h2>
     <button class="back-btn">Försök igen</button>
     `
   let backBtn = document.querySelector(".back-btn");
   backBtn.addEventListener("click", () => {
-    searchPlanet();
+    inputSearchPlanet();
 })
 }
 
-
-generateClickableUI()
+renderPlanetsToUI()
 addEventListenerToPlanetSearch()
 
